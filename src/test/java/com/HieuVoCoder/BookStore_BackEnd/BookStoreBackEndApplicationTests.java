f92541269abf98ddcbce414c89cd53fcc5ee78ec
hieuvolaptrinh <vndhieuak@gmail.com>
2025-03-02 10:58:36 +0700

    create new project

---
+++ src/test/java/com/HieuVoCoder/BookStore_BackEnd/BookStoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HieuVoCoder.BookStore_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
